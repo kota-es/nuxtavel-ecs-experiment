bbbb1b3501041dd36cf1dcc8d0cd1e16dce9189e
kota-es <info@k-3110.com>
2021-08-14 11:03:40 +0900

    テスト通信実装

--- frontend/pages/index.vue
+++ frontend/pages/index.vue
@@ -1,7 +1,23 @@
 <template>
-  <Tutorial/>
+  <div>
+    <button @click="test()">test</button>
+    <div>{{ message }}</div>
+  </div>
 </template>
 
 <script>
-export default {}
+export default {
+  data() {
+    return {
+      message: 'default'
+    }
+  },
+  methods: {
+    async test() {
+      const res = await this.$axios.get('/api')
+      this.message = res.data
+      console.log(res.data)
+    }
+  }
+}
 </script>
